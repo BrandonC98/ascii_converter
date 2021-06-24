f985150259093f1bbad78cb508f618575a844342
Brandon Campbell <brandoncampbell98@hotmail.co.uk>
2021-06-24 16:01:24 +0100

    Adds fix for cross platoform output

--- examples/conversion.rs
+++ examples/conversion.rs
@@ -1,13 +1,20 @@
 use ascii_converter::*;
+use std::io::*;
 
 fn main() {
 
-    let mut input = String::new();
-    println!("Enter text: ");
-    std::io::stdin().read_line(&mut input).unwrap();
-    input.truncate(input.len() -1);
+    let mut name = String::new();
+    print!("Enter name: ");
+    stdout().flush();
+    //reads user input and assigns it to the name variable
+    stdin().read_line(&mut name).unwrap();
 
-    println!("* {} in Binary: {:?}", input, string_to_binary(&input).unwrap());
-    println!("* {} in Decimal: {:?}", input, string_to_decimals(&input).unwrap());
+    let name = name.trim();
+
+    //outputs the binary representation
+    println!("* {} in Binary: {:?}", name, string_to_binary(&name).unwrap());
+    
+    //outputs the decimal representation
+    println!("* {} in Decimal: {:?}", name, string_to_decimals(&name).unwrap());
 
 }
